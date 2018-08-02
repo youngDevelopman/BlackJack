9599f51ad83c819e71c043e4a745fcb62c0650ab
youngDevelopman <nazarmazurik99@gmail.com>
2018-08-02 10:29:33 +0300

    Start menu added

---
+++ BlackJackGame/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using BlackJackGame.Game;
+using System.Web.Mvc;
+
+namespace BlackJackGame.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult GameStartMenu()
+        {
+            UserGameOptions userGameOptions = new UserGameOptions();
+            return View(userGameOptions);
+        }
+
+        [HttpPost]
+        public ActionResult GameStartMenu(UserGameOptions gameOptions)
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ BlackJackGame/Game/UserGameOptions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BlackJackGame.Game
+{
+    public class UserGameOptions
+    {
+        [Display(Name = "Number of bots")]
+        public int NumberOfPlayers { get; set; }
+
+        [Display(Name = "Your name")]
+        public string PlayerName { get; set; }       
+
+    }
+}
